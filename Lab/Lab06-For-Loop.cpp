d0940ca70a8badab69eb4763763bfe67d55ae8ab
your name <sriwljitthitima@gmail.com>
2023-08-03 14:28:00 +0700

    uppp

--- Lab/Lab06-For-Loop.cpp
+++ Lab/Lab06-For-Loop.cpp
@@ -18,8 +18,23 @@
 #include <stdio.h>
 
 int main() {
-
-    //--| YOUR CODE HERE
+    int i  ;
+    int N  ;
+    printf("Enter value: " );
+    scanf("%d",&N);
+    if(N%2 ==1){
+        printf("Series: ");
+        for ( i = 1 ; i <= N ; i += 2 ) {
+        printf ("%d ",i);
+    }
+    }else {
+        for(i=N ; i>=0 ; i-=2){
+            printf ("%d ",i);
+        }
+    }
+    
+   
+   
 
     return 0 ;
 }//end main function
